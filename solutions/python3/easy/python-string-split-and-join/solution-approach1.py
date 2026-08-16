def5e13450525f3ca31e90d166497533f8ee2abd
Shubham Mangnale <shubhammangnale93@gmail.com>
2026-08-16 23:58:23 +0530

    ✅ String Split and Join [python3] — approach1

---
+++ solutions/python3/easy/python-string-split-and-join/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+# Problem     String Split and Join
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-16, 11:58 p.m.
+# ──────────────────────────────────────────────────
+
+def split_and_join(line):
+    # write your code here
+    return "-".join(line.split(" "))
+
